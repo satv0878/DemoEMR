99134fc9156f98112ce733a089dfea2b6482c447
unknown <verleger.t@gmail.com>
2019-05-28 12:27:04 +0200

    Version update 1.0.8 first change

--- src/components/VueDataTable.vue
+++ src/components/VueDataTable.vue
@@ -14,7 +14,7 @@
   </div>
    <v-spacer></v-spacer>
   <div class="text-xs-right">
-    <span class="white--text"  style=" font-size:large;">Demo EMR 1.2</span>
+    <span class="white--text"  style=" font-size:large;">Demo EMR 1.3</span>
   </div>
 
 </v-toolbar>
